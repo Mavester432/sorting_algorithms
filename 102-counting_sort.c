2ba0a38acec63073dabcc664df65a496c4bc3dff
Mavester432 <mavester432@.gmail.com>
2024-02-20 17:50:33 -0800

    102-counting_sort.c

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,6 +1,25 @@
 #include "sort.h"
 
 /**
+ * get_max - Get the maximum value in an array of integers.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Return: The maximum integer in the array.
+ */
+int get_max(int *array, int size)
+{
+        int max, i;
+
+        for (max = array[0], i = 1; i < size; i++)
+        {
+        if (array[i] > max)
+        max = array[i];
+        }
+return (max);
+}
+
+/**
  * counting_sort - Sort an array of integers in ascending order
  *                 using the counting sort algorithm.
  * @array: An array of integers.
@@ -45,23 +64,4 @@ void counting_sort(int *array, size_t size)
 
 	free(sorted);
 	free(count);
-}
-
-/**
- * get_max - Get the maximum value in an array of integers.
- * @array: An array of integers.
- * @size: The size of the array.
- *
- * Return: The maximum integer in the array.
- */
-int get_max(int *array, int size)
-{
-	int max, i;
-
-	for (max = array[0], i = 1; i < size; i++)
-	{
-	if (array[i] > max)
-	max = array[i];
-	}
-return (max);
 }
